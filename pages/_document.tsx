90710ba659d72eb687da1f8d3ed630d2ab98e59b
Patrick Scully <patrick@possibleweb.com>
2021-04-07 21:23:49 -0400

    remove emotion from document file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { extractCritical } from "@emotion/server";
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    const page = await ctx.renderPage();
-    const styles = extractCritical(page.html);
-    return { ...initialProps, ...page, ...styles };
-  }
-
   render() {
     return (
-      <Html lang="en">
-        <Head>
-          <style
-            data-emotion-css={this.props.ids.join(" ")}
-            dangerouslySetInnerHTML={{ __html: this.props.css }}
-          />
-        </Head>
+      <Html lang='en'>
+        <Head />
         <body>
           <Main />
           <NextScript />
