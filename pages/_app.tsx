8085aa6e4fb1db2438ba6edd56f765aacd6a7e4a
Patrick Scully <patrick@possibleweb.com>
2021-04-12 09:58:12 -0400

    add contentful

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,19 @@ import { AppProps } from "next/app";
 import "../styles/index.css";
 import { GlobalStyles } from "twin.macro";
 // import "../util/analytics";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
